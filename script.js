81f382ec05b48d7954a75d625a1a7beb32f0caf6
Ambikesh Srivastava <abhay2004raj15@gmail.com>
2024-11-08 22:14:39 +0530

    Update3

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-console.log("Ambikesh is coder")
-console.log(2+4)
+let marks = [85, 97, 44, 37, 76, 60]
+
+n = marks.length
+
+console.log(n)
+let sum = 0
+
+for (let i = 0; i < n; i++){
+    sum = (sum + marks[i]) 
+    avg = sum / n
+}
+console.log(sum)
+console.log(avg)
